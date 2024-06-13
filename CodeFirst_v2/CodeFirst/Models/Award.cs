214f304af00ae71d80ee814936934af0618d7ea4
s27799 <s27799@pjwstk.edu.pl>
2024-06-13 15:48:26 +0200

    removed non related files

--- CodeFirst_v2/CodeFirst/Models/Award.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodeFirst.Models;
-
-public class Award
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    
-    public ICollection<BookAward> BookAwards { get; set; } = new HashSet<BookAward>();
-}
